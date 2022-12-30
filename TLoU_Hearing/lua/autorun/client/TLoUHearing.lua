b25015d3b5d76c6000337a1996eed99608481ff9
Jacket <97692547+Jacket235@users.noreply.github.com>
2022-12-31 00:14:08 +0100

    Update TLoUHearing.lua

--- TLoU_Hearing/lua/autorun/client/TLoUHearing.lua
+++ TLoU_Hearing/lua/autorun/client/TLoUHearing.lua
@@ -5,32 +5,36 @@ CreateClientConVar("hearingability_cooldown", "1", true, false)
 local time = CurTime()
 local target = {}
 
+keyBind = GetConVar("hearingability"):GetString()
+maxTargetDist = GetConVar("hearingability_distance"):GetInt()
+abilityCooldown = GetConVar("hearingability_cooldown"):GetInt()
+
 hook.Add( "PreDrawHalos", "addingGlow", function()
 
     for _, ent in ipairs(ents.FindByClass('npc_*')) do
         local dist = LocalPlayer():EyePos():DistToSqr(ent:GetPos() + ent:OBBCenter())
-        if dist > GetConVar("hearingability_distance"):GetInt() ^2 then 
+        if dist > maxTargetDist ^2 then 
             if target[ent] then 
                 target[ent] = nil 
             end
         else
-            local alphaIndi = 1 - dist / GetConVar("hearingability_distance"):GetInt() ^2
+            local alphaIndi = 1 - dist / maxTargetDist ^2
             target[ent] = Color(255, 255, 255, alphaIndi * 70)
         end
     end
     for _, ply in ipairs(player.GetAll()) do
         local dist = LocalPlayer():EyePos():DistToSqr(ply:GetPos() + ply:OBBCenter())
-        if dist > GetConVar("hearingability_distance"):GetInt() ^2 then 
+        if dist > maxTargetDist ^2 then 
             if target[ply] then 
                 target[ply] = nil 
             end
         else
-            local alphaIndi = 1 - dist / GetConVar("hearingability_distance"):GetInt() ^2
+            local alphaIndi = 1 - dist / maxTargetDist ^2
             target[ply] = Color(255, 255, 255, alphaIndi * 70)
         end
     end
 
-	if input.IsKeyDown( input.GetKeyCode(GetConVar("hearingability"):GetString()) ) && CurTime() - time > GetConVar("hearingability_cooldown"):GetInt() then
+	if input.IsKeyDown( input.GetKeyCode(keyBind) ) && CurTime() - time > abilityCooldown then
 		alpha = math.Approach( alpha, 1, FrameTime() / 1.20 )
 		local color = Color( 255, 255, 255, 70 * alpha )
 
@@ -52,7 +56,7 @@ hook.Add( "PreDrawHalos", "addingGlow", function()
 	end
 end )
 hook.Add( "PlayerButtonUp", "shittingMyPants", function( _, button )
-	if button == input.GetKeyCode(GetConVar("hearingability"):GetString()) && CurTime() - time > GetConVar("hearingability_cooldown"):GetInt() then
+	if button == input.GetKeyCode(keyBind) && CurTime() - time > abilityCooldown then
 		time = CurTime()
 		alpha = 0
 	end
