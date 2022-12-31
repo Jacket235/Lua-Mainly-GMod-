9fc38d50bb982d11d5d96f5aadd6abf11f532c98
Jacket <97692547+Jacket235@users.noreply.github.com>
2022-12-31 02:26:12 +0100

    Update cl_tlouhud.lua

--- TLoU2_HUD/lua/HUD/client/cl_tlouhud.lua
+++ TLoU2_HUD/lua/HUD/client/cl_tlouhud.lua
@@ -3,15 +3,15 @@
 local function LoadFonts()
 	surface.CreateFont("Primary", {
 		font = "DIN Next LT Arabic Medium",
-		size = 50,
+		size = ScrW() * .026042,
 	} )
 	surface.CreateFont( "Ammo", {
 		font = "DIN Next LT Arabic Medium",
-		size = 45,
+		size = ScrW() * .02344,
 		} )	
 	surface.CreateFont("Text", {
 		font = "DIN Next LT Arabic Medium",
-		size = 20,
+		size = ScrW() * .01042,
 	} )
 end
 
@@ -90,67 +90,67 @@ local rpg = Material("materials/TLoU2Icons/RPG.png")
 local function weaponsAndAmmo()
 	local ply = LocalPlayer()
     surface.SetDrawColor(0, 0, 0, 200)
-    surface.DrawRect(ScrW() / 2 + 580, ScrH() / 2 + 220, 205 , 75)
+    surface.DrawRect(ScrW() * .80209, ScrH() * 0.7046, ScrW() * .107 , ScrH() * .06944)
     surface.SetDrawColor(150, 150, 150, 100)
-    surface.DrawRect(ScrW() / 2 + 580, ScrH() / 2 + 218, 205, 2)
+    surface.DrawRect(ScrW() * .80209, ScrH() * 0.7046, ScrW() * .10678, ScrH() * .00185186)
     
     local currWep = ply:GetActiveWeapon()
 
     if (currWep:IsValid()) then
         if(currWep:Clip1() != -1 and currWep:GetPrimaryAmmoType() != nil) then
-            draw.SimpleText(currWep:Clip1(), "Primary", ScrW() / 2 + 698, ScrH()/ 2 + 248, Color( 255, 255, 255, 255 ), 0, 0)
-            draw.SimpleText("|", "Text", ScrW() / 2 + 730, ScrH() / 2 + 265, Color(180, 180, 180, 255), 0, 0)
-            draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() / 2 + 736, ScrH() / 2 + 251, Color(180, 180, 180, 255), 0, 0)
+            draw.SimpleText(currWep:Clip1(), "Primary", ScrW() * .8635421, ScrH() * .72963, Color( 255, 255, 255, 255 ), 0, 0)
+            draw.SimpleText("|", "Text", ScrW() * .88021, ScrH() * .745371, Color(180, 180, 180, 255), 0, 0)
+            draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() * .883334, ScrH() * .73241, Color(180, 180, 180, 255), 0, 0)
             local k = 0
             if currWep:Clip1() <= 28 then
                 for i = 1, currWep:Clip1() do
                     k = k + 4
                     surface.SetDrawColor(255,255,255,255)
-                    surface.DrawRect(ScrW() / 2 + 582 + k, ScrH() / 2 + 280, 2, 8)
+                    surface.DrawRect(ScrW() * .803125 + k, ScrH() * .75926, ScrW() * 0.001042, ScrH() * 0.00741) // k is a problem
                 end
             else
                 for i = 1, 28 do
                     k = k + 4
                     surface.SetDrawColor(255,255,255,255)
-                    surface.DrawRect(ScrW() / 2 + 582 + k, ScrH() / 2 + 280, 2, 8)
+                    surface.DrawRect(ScrW() * .803125 + k, ScrH() * .75926, ScrW() * 0.001042, ScrH() * 0.00741) // k is a problem
                 end
             end
             if currWep:GetPrimaryAmmoType() == 1 then
                 surface.SetMaterial(ar2)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 220,68,68)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .703704, ScrW() * .03542, ScrW() * .03542)
             elseif currWep:GetPrimaryAmmoType() == 3 then
                 surface.SetMaterial(pistol)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 220,55,55)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .703704, ScrW() * .02865, ScrW() * .02865)
             elseif currWep:GetPrimaryAmmoType() == 4 then
                 surface.SetMaterial(smg)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 220,67,67)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .703704, ScrW() * .034897, ScrW() * .034897)
             elseif currWep:GetPrimaryAmmoType() == 5 then
                 surface.SetMaterial(revolver)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 220,67,67)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .703704, ScrW() * .034897, ScrW() * .034897)
             elseif currWep:GetPrimaryAmmoType() == 7 then
                 surface.SetMaterial(shotgun)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 565,ScrH() / 2 + 185,140,140)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .671297, ScrW() * .0729167, ScrW() * .0729167)
             elseif currWep:GetPrimaryAmmoType() == 6 then
                 surface.SetMaterial(crossbow)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 190,110,110)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .675926, ScrW() * .057292, ScrW() * .057292)
             end 
         else
             if currWep:GetPrimaryAmmoType() == 10 then
                 surface.SetMaterial(grenade)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 225,67,67)
-                draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() / 2 + 740, ScrH() / 2 + 250, Color(255,255,255,255), 0, 0)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .70834, ScrW() * .034897, ScrW() * .034897)
+                draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() * .88542, ScrH() * .73149, Color(255,255,255,255), 0, 0)
              elseif currWep:GetPrimaryAmmoType() == 8 then
                 surface.SetMaterial(rpg)
                 surface.SetDrawColor(255,255,255,255)
-                surface.DrawTexturedRect(ScrW() / 2 + 590,ScrH() / 2 + 210,95,95)
-                draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() / 2 + 740, ScrH() / 2 + 250, Color(255,255,255,255), 0, 0)
+                surface.DrawTexturedRect(ScrW() * .80729, ScrH() * .69445, ScrW() * .04948, ScrW() * .04948)
+                draw.SimpleText(ply:GetAmmoCount(currWep:GetPrimaryAmmoType()), "Ammo", ScrW() * .88542, ScrH() * .73149, Color(255,255,255,255), 0, 0)
             end
         end
     end
@@ -170,7 +170,7 @@ local function healthBar()
     smoothHP2 = math.Approach(smoothHP2,((hp / maxHp) * 100) * 2.25, FrameTime() / 0.0175)
 
     surface.SetDrawColor(100,100,100,255)
-    draw.JRing(ScrW() / 2 + 750, ScrH() / 2 + 260, 75, 8, 2, 225)
+    draw.JRing(ScrW() * .89063, ScrH() * .7408, ScrH() * .070291, ScrW() * .0041667, 2, 225)
 
     for i = 0, 180, 45 do
         function HpAng(i, maxAng)
@@ -192,9 +192,9 @@ local function healthBar()
             return smoothHP2
         end
         surface.SetDrawColor(149,21,37,255)
-        draw.JRing(ScrW() / 2 + 750, ScrH() / 2 + 260, 75, 8, i + 2, math.Clamp(HpSmoothAng(i,45), 0, 225))
+        draw.JRing(ScrW() * .89063, ScrH() * .7408, ScrH() * .070291, ScrW() * .0041667, i + 2, math.Clamp(HpSmoothAng(i,45), 0, 225))
         surface.SetDrawColor(225,225,225,255)
-        draw.JRing(ScrW() / 2 + 750, ScrH() / 2 + 260, 75, 8, i + 2, math.Clamp(HpAng(i, 45),0,225))
+        draw.JRing(ScrW() * .89063, ScrH() * .7408, ScrH() * .070291, ScrW() * .0041667, i + 2, math.Clamp(HpAng(i, 45),0,225))
     end 
 end 
 
@@ -206,7 +206,7 @@ local function armorBar()
 
     surface.SetDrawColor(30,30,30,150)
     if armor > 0 then
-        draw.JRing(ScrW() / 2 + 750, ScrH() / 2 + 260, 87, 8, 2, 225)
+        draw.JRing(ScrW() * .89063, ScrH() * .7408, ScrH() * .080555, ScrW() * .0041667, 2, 225)
     end
     
     for k = 0, 180, 45 do
@@ -220,7 +220,7 @@ local function armorBar()
             return smoothArmor
         end
         surface.SetDrawColor(0,0,225,255)
-        draw.JRing(ScrW() / 2 + 750, ScrH() / 2 + 260, 87, 8, k + 2, ArmorAng(k, 45))
+        draw.JRing(ScrW() * .89063, ScrH() * .7408, ScrH() * .080555, ScrW() * .0041667, k + 2, ArmorAng(k, 45))
     end 
 end
 
